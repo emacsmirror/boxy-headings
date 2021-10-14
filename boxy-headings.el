5c40f1a1f2689c35ba9bf34c1e43ad72d00b7e25
Tyler Grinn <tylergrinn@gmail.com>
2021-10-14 10:17:57 -0400

    Bump version

--- boxy-headings.el
+++ boxy-headings.el
@@ -3,7 +3,7 @@
 ;; Copyright (C) 2021 Free Software Foundation, Inc.
 
 ;; Author: Tyler Grinn <tylergrinn@gmail.com>
-;; Version: 2.0.0
+;; Version: 2.1.0
 ;; File: boxy-headings.el
 ;; Package-Requires: ((emacs "26.1") (boxy "1.0") (org "9.3"))
 ;; Keywords: tools
